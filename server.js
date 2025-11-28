4da572a4defc99a31b121c51bef2897445edd152
Adithya <adithya.4818@gmail.com>
2025-11-28 15:16:29 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+const app=express()
+const PORT=3000
+
+app.use(express.json())
+app.use(express.static("public"))
+app.use(express.urlencoded({extended:true}))
+
+const blog_posts=[];
+app.get('/',(req,res)=>{
+    res.render("index.ejs")
+})
+app.get('/posts',(req,res)=>{
+    console.log(blog_posts)
+    res.render("posts.ejs",{blog_posts})
+})
+app.post('/add',(req,res)=>{
+    const data=req.body
+    blog_posts.push(data)
+})
+
+app.listen(PORT,()=>{
+    console.log(`listening on port ${PORT}`)
+})
